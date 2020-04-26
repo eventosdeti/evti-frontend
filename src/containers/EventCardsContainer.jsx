78e0029dd90b6b80c014bc7c0ed403812966c533
Allef Gomes <allefgalmeida@gmail.com>
2020-04-26 00:36:49 -0300

    Remove unecessary imports

--- src/containers/EventCardsContainer.jsx
+++ src/containers/EventCardsContainer.jsx
@@ -6,9 +6,6 @@ import EventCardContainer from "./EventCardContainer";
 
 import ChangeViewButtonsContainer from "./ChangeViewButtonsContainer";
 import { useCardsContext } from "../contexts/Cards";
-import useFetch from "../hooks/useFetch";
-
-const { REACT_APP_TRELLO_BOARD_JSON_URI } = process.env;
 
 const eventCardRender = (card) => (
   <EventCardContainer key={card.id} {...card} />
