65b5cbb13175357792d58c35a0c072737acde183
ryanlima <ryanlima08021997@gmail.com>
2021-03-19 15:33:59 -0300

    Ajusta usekeyPress

--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -1,34 +1,23 @@
 import React from "react";
 
 export const useKeyPress = (targetKey, functionOnKey) => {
-  const [keyPressed, setKeyPressed] = React.useState(false);
-
-  function downHandler({ key }) {
-    if (key === targetKey) {
-      setKeyPressed(true);
-    }
-  }
+  // const [keyPressed, setKeyPressed] = React.useState(false);
 
   const upHandler = ({ key }) => {
     if (key === targetKey) {
       functionOnKey();
-      setKeyPressed(false);
     }
   };
 
   React.useEffect(() => {
-    window.addEventListener("keydown", downHandler);
     window.addEventListener("keyup", upHandler);
 
     return () => {
-      window.removeEventListener("keydown", downHandler);
       window.removeEventListener("keyup", upHandler);
     };
 
     // eslint-disable-next-line
   }, []);
-
-  return keyPressed;
 };
 
 export default useKeyPress;
